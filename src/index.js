d7874e33f69f474a1bb2280798846b277049a59c
Tobias Reich <tobias@electerious.com>
2024-12-24 16:37:13 +0100

    Switch to js modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   importOrder: ['<BUILTIN_MODULES>', '<THIRD_PARTY_MODULES>', '', '^[./]'],
   plugins: ['@ianvs/prettier-plugin-sort-imports'],
   printWidth: 120,
