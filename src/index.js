3592ae4a4779d831617304830ef8969e0eb70052
Tobias Reich <tobias@electerious.com>
2025-08-02 17:22:22 +0200

    Format CSS and package.json, change plugin for imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default {
-  importOrder: ['<BUILTIN_MODULES>', '<THIRD_PARTY_MODULES>', '', '^[./]'],
-  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-css-order', 'prettier-plugin-packagejson'],
   printWidth: 120,
   quoteProps: 'consistent',
   semi: false,
