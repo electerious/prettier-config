890ae4306bd44dd12f062906637ef4a498cb8144
Tobias Reich <tobias@electerious.com>
2026-09-17 18:01:12 +0200

    Added prettier-plugin-jsdoc

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 
 ## [Unreleased]
 
+### Added
+
+- `prettier-plugin-jsdoc`
+
 ### Changed
 
 - Require Node.js >=24.15.0, dropping support for Node.js 22
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 export default {
   endOfLine: 'auto',
-  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-css-order', 'prettier-plugin-packagejson'],
+  plugins: [
+    'prettier-plugin-jsdoc',
+    'prettier-plugin-organize-imports',
+    'prettier-plugin-css-order',
+    'prettier-plugin-packagejson',
+  ],
   printWidth: 120,
   quoteProps: 'consistent',
   semi: false,
